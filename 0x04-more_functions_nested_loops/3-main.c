6564ca22c6f17f2df11e06786b3fafe99a74f2ca
deeoloo <doreenoloo2@gmail.com>
2023-02-23 06:51:04 -0800

    3-main.c

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - checks my code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
-    print_numbers();
-    return (0);
+	print_numbers();
+	return (0);
 }
